02e58a2277670bcf16f6cbf8a197da7d1a1bc998
Alexander <goryatov.a@gmail.com>
2017-05-10 04:06:56 +0300

    two update

---
+++ helpMe/helpMe/helpMe/helpMe/checkBox.swift
@@ -0,0 +1,21 @@
+//
+//  checkBox.swift
+//  helpMe
+//
+//  Created by Александр on 09.05.17.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import UIKit
+
+class checkBox: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ helpMe/helpMe/helpMe/helpMe/tapGesture.swift
@@ -0,0 +1,9 @@
+//
+//  tapGesture.swift
+//  helpMe
+//
+//  Created by Александр on 09.05.17.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
